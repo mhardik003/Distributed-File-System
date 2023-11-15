233e38cf23d8c1eb310cb2d5bec089567cb1395d
ArjunDosajh <arjundosajh100@gmail.com>
2023-11-15 19:18:42 +0530

    Client and storage servers find ports dynamically

--- Client Server/main.c
+++ Client Server/main.c
@@ -5,9 +5,49 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
 #define SERVER_PORT 8080
-#define CLIENT_NM_PORT 8082
+int START_PORT = 8000;
+int CLIENT_NM_PORT;
+int CLIENT_SS_PORT;
+
+int findAvailablePort() {
+    int test_sock;
+    struct sockaddr_in test_addr;
+    int port = START_PORT;
+
+    while (port < 65535) {
+        test_sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (test_sock < 0) {
+            perror("Socket creation error in findAvailablePort");
+            exit(EXIT_FAILURE);
+        }
+
+        memset(&test_addr, 0, sizeof(test_addr));
+        test_addr.sin_family = AF_INET;
+        test_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        test_addr.sin_port = htons(port);
+
+        if (bind(test_sock, (struct sockaddr *)&test_addr, sizeof(test_addr)) < 0) {
+            if (errno == EADDRINUSE) {
+                // Port is already in use, try next port
+                close(test_sock);
+                port++;
+            } else {
+                perror("bind failed in findAvailablePort");
+                close(test_sock);
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            // Found an available port
+            START_PORT = port + 1;
+            close(test_sock);
+            return port;
+        }
+    }
+    return -1; // No available port found
+}
 
 int createClientSocket() {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -74,8 +114,18 @@ void print_DFS_features() {
 }
 
 int main() {
+    CLIENT_NM_PORT = findAvailablePort();
+    CLIENT_SS_PORT = findAvailablePort();
+    if (CLIENT_NM_PORT == -1 || CLIENT_SS_PORT == -1) {
+        printf("No available port found. Exiting.\n");
+        return -1;
+    }
+
+    printf("\nClient Port for communication with naming server: %d\n", CLIENT_NM_PORT);
+    printf("Client Port for communication with storage server: %d\n\n", CLIENT_SS_PORT);
     int sock;
     struct sockaddr_in serv_addr, cli_addr;
+    char server_ip[INET_ADDRSTRLEN]; // Buffer for the IP address
 
     cli_addr.sin_family = AF_INET;
     cli_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -84,10 +134,18 @@ int main() {
     sock = createClientSocket();
     bindClientSocket(sock, &cli_addr);
 
+    printf("Enter the IP address of the Naming Server: ");
+    fgets(server_ip, sizeof(server_ip), stdin);
+    server_ip[strcspn(server_ip, "\n")] = 0; // Remove newline character if present
+
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(SERVER_PORT);
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
 
     connectToServer(sock, &serv_addr);
     print_DFS_features();
@@ -96,6 +154,7 @@ int main() {
         char message[1024];
         printf("Enter your message: ");
         fgets(message, 1024, stdin);
+        message[strcspn(message, "\n")] = 0; // Remove newline character if present
         sendMessage(sock, message);
         readMessage(sock); // Read confirmation message from server
     }
--- Storage Server/main.c
+++ Storage Server/main.c
@@ -5,9 +5,49 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
 #define SERVER_PORT 8081
-#define SS_NM_PORT 8083
+int START_PORT = 5000;
+int SS_NM_PORT;
+int SS_Client_PORT;
+
+int findAvailablePort() {
+    int test_sock;
+    struct sockaddr_in test_addr;
+    int port = START_PORT;
+
+    while (port < 65535) {
+        test_sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (test_sock < 0) {
+            perror("Socket creation error in findAvailablePort");
+            exit(EXIT_FAILURE);
+        }
+
+        memset(&test_addr, 0, sizeof(test_addr));
+        test_addr.sin_family = AF_INET;
+        test_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        test_addr.sin_port = htons(port);
+
+        if (bind(test_sock, (struct sockaddr *)&test_addr, sizeof(test_addr)) < 0) {
+            if (errno == EADDRINUSE) {
+                // Port is already in use, try next port
+                close(test_sock);
+                port++;
+            } else {
+                perror("bind failed in findAvailablePort");
+                close(test_sock);
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            // Found an available port
+            START_PORT = port + 1;
+            close(test_sock);
+            return port;
+        }
+    }
+    return -1; // No available port found
+}
 
 int createSSSocket() {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -46,8 +86,18 @@ void readMessage(int sock) {
 }
 
 int main() {
+    SS_NM_PORT = findAvailablePort();
+    SS_Client_PORT = findAvailablePort();
+    if (SS_NM_PORT == -1 || SS_Client_PORT == -1) {
+        printf("No available port found. Exiting.\n");
+        return -1;
+    }
+
+    printf("\nStorage Server Port for communication with naming server: %d\n", SS_NM_PORT);
+    printf("Storage Server Port for communication with client: %d\n\n", SS_Client_PORT);
     int sock;
     struct sockaddr_in serv_addr, ss_addr;
+    char server_ip[INET_ADDRSTRLEN]; // Buffer for the IP address
 
     ss_addr.sin_family = AF_INET;
     ss_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -56,10 +106,18 @@ int main() {
     sock = createSSSocket();
     bindSSSocket(sock, &ss_addr);
 
+    printf("Enter the IP address of the Naming Server: ");
+    fgets(server_ip, sizeof(server_ip), stdin);
+    server_ip[strcspn(server_ip, "\n")] = 0; // Remove newline character if present
+
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(SERVER_PORT);
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
 
     connectToServer(sock, &serv_addr);
 
@@ -67,6 +125,7 @@ int main() {
         char message[1024];
         printf("Enter your message: ");
         fgets(message, 1024, stdin);
+        message[strcspn(message, "\n")] = 0; // Remove newline character if present
         sendMessage(sock, message);
         readMessage(sock); // Read confirmation message from server
     }
@@ -74,3 +133,4 @@ int main() {
     close(sock);
     return 0;
 }
+
