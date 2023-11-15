4135d27c151a56624e68595c4e5684ea0d3b1e00
ArjunDosajh <arjundosajh100@gmail.com>
2023-11-15 23:13:53 +0530

    Saved IP of the clients and storage servers in the naming server

--- Naming Server/main.c
+++ Naming Server/main.c
@@ -4,9 +4,15 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include "utils.h"
 #include "Utils/hashmap.h"
 #include <pthread.h>
+
+typedef struct {
+    int socket_desc;
+    char ip_address[INET_ADDRSTRLEN]; // ip of the client or storage server
+} connection_info;
 
 #define NM_Client_PORT 8080 // port for naming server's communication with the client
 #define NM_SS_PORT 8081 // port for naming server's communication with the storage server
@@ -40,8 +46,7 @@ void startListening(int server_fd)
     }
 }
 
-int acceptConnection(int server_fd, struct sockaddr_in *address)
-{
+int acceptConnection(int server_fd, struct sockaddr_in *address, char *ip_buffer) {
     int addrlen = sizeof(*address);
     int new_socket = accept(server_fd, (struct sockaddr *)address, (socklen_t *)&addrlen);
     if (new_socket < 0)
@@ -49,6 +54,11 @@ int acceptConnection(int server_fd, struct sockaddr_in *address)
         perror("accept");
         exit(EXIT_FAILURE);
     }
+
+    // Copy the IP address to the provided buffer
+    strncpy(ip_buffer, inet_ntoa(address->sin_addr), INET_ADDRSTRLEN);
+    ip_buffer[INET_ADDRSTRLEN - 1] = '\0';  // Null-terminate the string
+
     return new_socket;
 }
 
@@ -103,13 +113,18 @@ char *readStorageServerMessage(int sock)
     return "Hey SS, how are you? -NM";
 }
 
-void *handleClientConnection(void *socket_desc) {
-    int sock = *(int*)socket_desc;
-    free(socket_desc);
+void *handleClientConnection(void *arg) {
+    connection_info *info = (connection_info *)arg;
+    int sock = info->socket_desc;
+    char ip_buffer[INET_ADDRSTRLEN];  // Buffer to store the IP address
+    strncpy(ip_buffer, info->ip_address, INET_ADDRSTRLEN);
+
+    free(info); // Free the allocated memory for the connection info
 
     while (1) {
         char *message = readClientMessage(sock);
         if (message == NULL || strcmp(message, "QUIT") == 0) {
+            printf("Storage Server [%s] disconnected\n", ip_buffer);
             break;
         }
         sendMessage(sock, message);
@@ -119,13 +134,18 @@ void *handleClientConnection(void *socket_desc) {
     return NULL;
 }
 
-void *handleStorageServerConnection(void *socket_desc) {
-    int sock = *(int*)socket_desc;
-    free(socket_desc);
+void *handleStorageServerConnection(void *arg) {
+    connection_info *info = (connection_info *)arg;
+    int sock = info->socket_desc;
+    char ip_buffer[INET_ADDRSTRLEN];
+    strncpy(ip_buffer, info->ip_address, INET_ADDRSTRLEN);
+
+    free(info);  // Free the allocated memory for the connection info
 
     while (1) {
         char *message = readStorageServerMessage(sock);
         if (message == NULL || strcmp(message, "QUIT") == 0) {
+            printf("Storage Server [%s] disconnected\n", ip_buffer);
             break;
         }
         sendMessage(sock, message);
@@ -140,11 +160,18 @@ void *listenForClients(void *arg) {
     int server_fd = *(int *)arg;
     while (1) {
         struct sockaddr_in address;
+        char ip_buffer[INET_ADDRSTRLEN];  // Buffer to store the IP address
+
         int *new_sock = malloc(sizeof(int));
-        *new_sock = acceptConnection(server_fd, &address);
+        *new_sock = acceptConnection(server_fd, &address, ip_buffer);
+        printf("Connected to client %s\n", ip_buffer);
+
         if (*new_sock >= 0) {
             pthread_t thread_id;
-            pthread_create(&thread_id, NULL, handleClientConnection, (void*) new_sock);
+            connection_info *info = malloc(sizeof(connection_info));
+            info->socket_desc = *new_sock;
+            strncpy(info->ip_address, ip_buffer, INET_ADDRSTRLEN);
+            pthread_create(&thread_id, NULL, handleClientConnection, (void*) info);
         }
     }
     return NULL;
@@ -154,14 +181,21 @@ void *listenForStorageServers(void *arg) {
     int server_fd = *(int *)arg;
     while (1) {
         struct sockaddr_in address;
+        char ip_buffer[INET_ADDRSTRLEN];  // Buffer to store the IP address
+
         int *new_sock = malloc(sizeof(int));
-        *new_sock = acceptConnection(server_fd, &address);
+        *new_sock = acceptConnection(server_fd, &address, ip_buffer);
+        printf("Connected to storage server %s\n", ip_buffer);
+
         if (*new_sock >= 0) {
             pthread_t thread_id;
-            pthread_create(&thread_id, NULL, handleStorageServerConnection, (void*) new_sock);
+            connection_info *info = malloc(sizeof(connection_info));
+            info->socket_desc = *new_sock;
+            strncpy(info->ip_address, ip_buffer, INET_ADDRSTRLEN);
+            
+            pthread_create(&thread_id, NULL, handleStorageServerConnection, (void*) info);
         }
     }
-    printf("stopped listening");
     return NULL;
 }
 
