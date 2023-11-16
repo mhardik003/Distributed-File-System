f98f6b64ff7e0de4e5bc4ca9e120fe3a41f4a07a
mhardik003 <mhardik2003@gmail.com>
2023-11-16 23:14:17 +0530

    Added comments to the SS_handler functions

--- Naming Server/Utils/SS_handler.h
+++ Naming Server/Utils/SS_handler.h
@@ -3,10 +3,19 @@
 
 #include <string.h>
 
-int parse_ssinit(char *init_message, char *ip_address) {
+int parse_ssinit(char *init_message, char *ip_address)
+{
   /*
   Parses the init message sent by the storage server
   and stores the information in the hashmap
+
+    Parameters:
+    char *init_message : init message sent by the storage server
+    char *ip_address : IP address of the storage server
+
+    Returns:
+    int : 1 if the parsing is successful, 0 otherwise
+
   The init message consists of
   * List of paths accessible by the storage server
   * IP address of the storage server
@@ -17,18 +26,21 @@ int parse_ssinit(char *init_message, char *ip_address) {
   int ss_nm_port, ss_client_port;
 
   token = strtok(init_message, "\n");
-  if (token == NULL || strcmp(token, "ssinit") != 0) {
+  if (token == NULL || strcmp(token, "ssinit") != 0)
+  {
     return 0;
   }
 
   token = strtok(NULL, "\n");
   if (token == NULL ||
-      sscanf(token, "%d %d", &ss_nm_port, &ss_client_port) != 2) {
+      sscanf(token, "%d %d", &ss_nm_port, &ss_client_port) != 2)
+  {
     return 0;
   }
 
   token = strtok(NULL, "\n");
-  while (token != NULL) {
+  while (token != NULL)
+  {
     ValueStruct vs = {ip_address, ss_nm_port, ss_client_port, 0, 0};
     insert(accessible_paths_hashmap, token,
            vs); // insert the path and the value struct in the hashmap for the
@@ -46,21 +58,46 @@ int parse_ssinit(char *init_message, char *ip_address) {
   return 1;
 }
 
-char *readStorageServerMessage(int sock, char *ip_address) {
+char *readStorageServerMessage(int sock, char *ip_address)
+{
+  /*
+    Function to read the message sent by the storage server
+
+    Parameters:
+    int sock : socket descriptor of the storage server
+    char *ip_address : IP address of the storage server
+
+    Returns:
+    char * : message sent by the storage server
+  */
+
   // printf("Now reading the message\n");
   char buffer[BUFFER_RECV_SIZE] = {0};
-  if (recv(sock, buffer, BUFFER_RECV_SIZE, 0) < 0) {
+  if (recv(sock, buffer, BUFFER_RECV_SIZE, 0) < 0)
+  {
     return "Error in receiving the message";
   }
   printf("Message received by SS : %s", buffer);
-  if (strncmp(buffer, "ssinit", 6) == 0) {
+  if (strncmp(buffer, "ssinit", 6) == 0)
+  {
     parse_ssinit(buffer, ip_address);
     return "ssinit done";
   }
   return "Hey SS, how are you? -NM";
 }
 
-void *handleStorageServerConnection(void *arg) {
+void *handleStorageServerConnection(void *arg)
+{
+  /*
+    Function to handle the connection with the storage server
+
+    Parameters:
+    void *arg : pointer to the connection_info struct
+
+    Returns:
+    void * : NULL
+  */
+
   connection_info *info = (connection_info *)arg;
   int sock = info->socket_desc;
   char ip_buffer[INET_ADDRSTRLEN];
@@ -70,7 +107,8 @@ void *handleStorageServerConnection(void *arg) {
 
   // while (1) {
   char *message = readStorageServerMessage(sock, ip_buffer);
-  if (message == NULL || strcmp(message, "QUIT") == 0) {
+  if (message == NULL || strcmp(message, "QUIT") == 0)
+  {
     printf("Storage Server [%s] disconnected\n", ip_buffer);
     close(sock);
     return NULL;
@@ -83,9 +121,21 @@ void *handleStorageServerConnection(void *arg) {
   return NULL;
 }
 
-void *listenForStorageServers(void *arg) {
+void *listenForStorageServers(void *arg)
+{
+  /*
+    Function to listen for connections from the storage servers
+
+    Parameters:
+    void *arg : pointer to the server_fd
+
+    Returns:
+    void * : NULL
+  */
+ 
   int server_fd = *(int *)arg;
-  while (1) {
+  while (1)
+  {
     struct sockaddr_in address;
     char ip_buffer[INET_ADDRSTRLEN]; // Buffer to store the IP address
 
@@ -93,7 +143,8 @@ void *listenForStorageServers(void *arg) {
     *new_sock = acceptConnection(server_fd, &address, ip_buffer);
     printf("Connected to storage server %s\n", ip_buffer);
 
-    if (*new_sock >= 0) {
+    if (*new_sock >= 0)
+    {
       pthread_t thread_id;
       connection_info *info =
           (connection_info *)malloc(sizeof(connection_info));
